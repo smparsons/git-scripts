df9ba302bbb60c21057fc6a486f93d96b08ee17d
Samuel Parsons <samuel.m.parsons@gmail.com>
2019-08-04 12:21:36 -0400

    Created setup file

---
+++ setup.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+CWD="$(cd -P -- "$(dirname -- "${BASH_SOURCE[0]}")" && pwd -P)"
+
+scripts_directory=$CWD/scripts
+
+chmod -R +x $scripts_directory
+ln -s $scripts_directory/cmtpush.sh /usr/local/bin/cmtpush
